687f72668990d82c23db6af4dc215148686f9629
savage <146668608+Rinattttt@users.noreply.github.com>
2024-10-15 13:25:06 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "C:\\Users\\Alena\\Desktop\\24_demo.txt"; 
+        try
+        {
+            string content = File.ReadAllText(filePath);
+            int maxLength = FindLongestXSequence(content);
+            Console.WriteLine($"Длина самой длинной последовательности символов 'X': {maxLength}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла ошибка: {ex.Message}");
+        }
+    }
+
+    static int FindLongestXSequence(string str)
+    {
+        int maxCount = 0; // Максимальная длина последовательности
+        int currentCount = 0; // Длина текущей последовательности
+
+        foreach (char c in str)
+        {
+            if (c == 'X')
+            {
+                currentCount++;
+            }
+            else
+            {
+                if (currentCount > maxCount)
+                {
+                    maxCount = currentCount;
+                }
+                currentCount = 0; // Сбрасываем счетчик
+            }
+        }
+
+        // Проверяем последнюю последовательность
+        if (currentCount > maxCount)
+        {
+            maxCount = currentCount;
+        }
+
+        return maxCount;
+    }
+}
